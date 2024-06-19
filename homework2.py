8ea20a85c3d416dac1a15d2502e4ba07141bd9b7
olgalvova2024 <olga.lvova1317@mail.ru>
2024-06-19 19:04:22 +0300

    практическое задание №2

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_homework_completed=12
+number_hours_used=1.5
+time_per_task=number_hours_used/number_homework_completed
+course='Pyton'
+print('Курс: ',course,', всего задач:',number_homework_completed, ', затрачено часов: ',
+      number_hours_used,', среднее время выполнения', time_per_task, 'часа')
